6b9d4cf0ab7161635ac2e14f11b5ed96744beacb
Ryan Morris <mryan.morris@gmail.com>
2023-06-15 05:12:34 -0500

    feat(landing-page): add simple UI for the landing page

--- test/quant_fitness_web/controllers/page_controller_test.exs
+++ test/quant_fitness_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule QuantFitnessWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert html_response(conn, 200) =~ "The ultimate fitness platform"
   end
 end
