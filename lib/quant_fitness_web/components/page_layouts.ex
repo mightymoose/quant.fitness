632589f860d361cb477f5d73665ec89010df9ba8
Ryan Morris <mryan.morris@gmail.com>
2023-06-27 13:08:33 +0000

    feat(workouts): show the exercises in the workout detail view

--- lib/quant_fitness_web/components/page_layouts.ex
+++ lib/quant_fitness_web/components/page_layouts.ex
@@ -201,7 +201,7 @@ defmodule QuantFitnessWeb.PageLayouts do
       </main>
 
       <aside class="fixed inset-y-0 right-0 hidden w-96 overflow-y-auto border-l border-gray-200 px-4 py-6 sm:px-6 lg:px-8 xl:block">
-        <!-- Secondary column (hidden on smaller screens) -->
+        <%= render_slot(@sidebar) %>
       </aside>
     </div>
     """
